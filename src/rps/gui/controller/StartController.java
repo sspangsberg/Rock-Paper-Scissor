6b545a5ea19446b2797492cf995606edc25a7eb2
Faustas Anulis <45820787+faustis1337@users.noreply.github.com>
2021-03-01 21:00:36 +0100

    transaction between windows

--- src/rps/gui/controller/StartController.java
+++ src/rps/gui/controller/StartController.java
@@ -1,4 +1,22 @@
 package rps.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
 public class StartController {
+    public void startGame(ActionEvent ae) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../view/GameView.fxml"));
+            Stage thisStage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+            thisStage.setScene(new Scene(root));
+        }
+        catch(Exception e)    {
+            e.printStackTrace();
+        }
+    }
 }
